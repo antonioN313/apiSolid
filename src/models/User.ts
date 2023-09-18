c9ba9bff18341c425cf8f864b119d3ade3348e01
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2023-09-18 16:11:45 -0300

    mvc e clas user

---
+++ src/models/User.ts
@@ -0,0 +1,10 @@
+export class User {
+
+    public readonly id: Number;
+    public name: string;
+    public email: string;
+    public password: string;
+    constructor(props: Omit<User,'id'>,id?: number) {
+        
+    }
+}
