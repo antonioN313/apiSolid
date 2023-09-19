348de2b0aac58b280583143d6827d392114ffdbf
Antonio Carlos Fernandes Gomes Filho <antoniocarlosfilho2003@gmail.com>
2023-09-19 14:21:00 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 const router = Router()
 
-router.post('/users',(request,response)=>{
-    return response.status(201).send;
+router.post('/users', (request, response) => {
+  return createUserController.handle(request, response);
 });
 
 export { Router }
